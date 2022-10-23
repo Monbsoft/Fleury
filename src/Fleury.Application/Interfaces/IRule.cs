574e711799ae560551a26c9b49c0c9e12e8cf791
oliver254 <oliver254@hotmail.fr>
2022-10-23 14:00:20 +0200

    #1 : Add calculator

---
+++ src/Fleury.Application/Interfaces/IRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monbsoft.Fleury.Application.Interfaces
+{
+    internal interface IRule
+    {
+    }
+}
